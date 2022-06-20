d367d483beb6d590e4eb7f5a82a1e718949674da
yeahmat <6matbub@gmail.com>
2022-06-19 19:20:42 -0700

    feat: output should be formattable to account for localization #25

---
+++ lib/calendar/src/index.interfaces.ts
@@ -0,0 +1,39 @@
+
+/** Interfaces */
+export type getDaysInMonthInterface = {
+  year: string;
+  months: string[];
+  current: string;
+}
+
+export type commonKeyValueInterface = {
+  [x: string]: string | string;
+}
+
+export type formatResponseInterface = {
+  [x: string]: {
+    count: number;
+    collection: commonKeyValueInterface;
+  };
+}
+
+export type formatResponseProps = {
+  collector: formatResponseInterface;
+  current: string;
+  months: string[];
+  year: string;
+}
+
+export type formatErrorResponseInterface = {
+  [x: string]: {
+    title?: string;
+    body: string;
+  };
+}
+
+export type formatDateInterface = {
+  year: string;
+  months: string[];
+  current: string;
+  day?: string;
+}
--- lib/calendar/src/index.ts
+++ lib/calendar/src/index.ts
@@ -6,83 +6,37 @@ import localeData from 'dayjs/plugin/localeData';
 dayjs.extend(localeData);
 dayjs().format();
 
-/** Interfaces */
-interface getDaysInMonthInterface {
-  year: string;
-  months: string[];
-  current: string;
-}
-
-interface commonKeyValueInterface {
-  [x: string]: string | string;
-}
-
-interface formatResponseInterface {
-  [x: string]: {
-    count: number;
-    collection: commonKeyValueInterface;
-  };
-}
-
-interface formatErrorResponseInterface {
-  [x: string]: {
-    title?: string;
-    body: string;
-  };
-}
+import {
+  getDaysInMonthInterface,
+  commonKeyValueInterface,
+  formatResponseInterface,
+  formatErrorResponseInterface,
+  formatDateInterface,
+  formatResponseProps
+} from './index.interfaces';
 
 /** Constants */
 export const months: string[] = dayjs.months().reverse(); // https://day.js.org/docs/en/i18n/listing-months-weekdays
 
 /** Methods */
-export const formatDate = ({
-  year,
-  months,
-  current,
-  day = '01',
-}: {
-  year: string;
-  months: string[];
-  current: string;
-  day?: string;
-}): string => {
+export const formatDate = ({year, months, current, day = '01'}: formatDateInterface, locale = ''): string => {
   const month = 12 - months.indexOf(current);
   const formattedDay = String(day).padStart(2, '0');
-  return `${year}-${month}-${formattedDay}`;
+  const formattedDate = new Date(`${year}-${month}-${formattedDay}`);
+  return new Intl.DateTimeFormat(locale && locale.length > 0 ? locale : 'en-US').format(formattedDate);
 };
 
-export const getDaysInMonth = ({
-  year,
-  months,
-  current,
-}: getDaysInMonthInterface): number =>
-  dayjs(formatDate({ year, months, current })).daysInMonth();
+export const getDaysInMonth = ({year, months, current}: getDaysInMonthInterface): number => (
+  dayjs(formatDate({year, months, current})).daysInMonth()
+);
 
-  export const formatResponse = ({
-  collector,
-  current,
-  months,
-  year,
-}: {
-  collector: formatResponseInterface;
-  current: string;
-  months: string[];
-  year: string;
-}): formatResponseInterface => {
+export const formatResponse = ({collector, current, months, year}: formatResponseProps, {locale}): formatResponseInterface => {
   /** Constants */
-  const daysInMonth: number = getDaysInMonth({ year, months, current });
+  const daysInMonth: number = getDaysInMonth({year, months, current});
   const daysOfMonthAsArray: number[] = [...new Array(daysInMonth).keys()];
 
-  const formatInnerReducerCallback = (
-    innerCollector: commonKeyValueInterface,
-    innerCurrent: number
-  ): commonKeyValueInterface => ({
-    [String(innerCurrent + 1)]: formatDate({
-      year,
-      months,
-      current,
-      day: String(innerCurrent + 1),
-    }),
+  const formatInnerReducerCallback = (innerCollector: commonKeyValueInterface, innerCurrent: number): commonKeyValueInterface => ({
+    [String(innerCurrent + 1)]: formatDate({year, months, current, day: String(innerCurrent + 1)}, locale),
     ...innerCollector,
   });
 
@@ -95,18 +49,19 @@ export const getDaysInMonth = ({
   };
 };
 
-export const calendar = (year: number | string): formatResponseInterface | formatErrorResponseInterface => {
+export const calendar = (year: number | string, options: {locale? : string | null}): formatResponseInterface | formatErrorResponseInterface => {
   if(String(year).length !== 4 || isNaN(parseFloat(String(year)))) {
     return ({
-      "error": {
-        "body": "invalid argument passed to `calendar('YYYY')`"
+      'error': {
+        'body': "invalid argument passed to `calendar('YYYY')`"
       }
-    })
+    });
   }
-  
+  const locale = (options && options.locale || null);
+
   return months.reduce(
     (collector: formatResponseInterface, current: string) =>
-      formatResponse({ year: String(year), months, current, collector }),
+      formatResponse({year: String(year), months, current, collector}, {locale}),
     {}
   );
-}
+};
